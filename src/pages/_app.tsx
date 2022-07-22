7b018729f5b282be6e636ef6016138c7c8e446c3
André Lusegardis <lusegardis@gmail.com>
2022-07-22 00:34:45 -0300

    added CssBaseLine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
